3c696104270e039434c251ab51cdaf10f6a13a3d
vlad gurbatov <vlad.gurbatov@gmail.com>
2023-03-27 05:48:26 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxL = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 138);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(100, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(12, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "x";
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(12, 30);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 3;
+            this.textBoxX.Text = "10";
+            // 
+            // textBoxL
+            // 
+            this.textBoxL.Location = new System.Drawing.Point(12, 112);
+            this.textBoxL.Name = "textBoxL";
+            this.textBoxL.Size = new System.Drawing.Size(100, 20);
+            this.textBoxL.TabIndex = 7;
+            this.textBoxL.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(9, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "l";
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(12, 72);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(100, 20);
+            this.textBoxN.TabIndex = 12;
+            this.textBoxN.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "n";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(118, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(670, 425);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxL;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+using System.Drawing.Printing;
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+        private int flag = -1;
+        private Bitmap bmp;
+        private double g = 9.81;
+        private int time = 10;
+        private double dt = 0.0005;
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            double x = double.Parse(textBoxX.Text);
+            double l = double.Parse(textBoxL.Text);
+            double n = double.Parse(textBoxN.Text);
+            flag *= -1;
+            startModulation(x, l, n);
+        }
+        private async void startModulation(double x, double l, double n)
+        {
+            double x0 = x;
+            double O = 0;
+            double w = Math.Sqrt(g / l);
+            double xgrad =40;
+            int x1;
+            int y1;
+            int y2  = pictureBox1.Height / 2;
+            int x2  = pictureBox1.Width / 2;
+            while(flag == 1)
+            {
+                bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                O += xgrad * dt;
+                x += (-2 * n * x - Math.Pow(w, 2) * Math.Sin(O)) * dt;
+
+                x1 = pictureBox1.Width / 2 + (int)Math.Round(x * Math.Sin(O));
+                y1 = pictureBox1.Height / 2 + (int)Math.Sqrt(Math.Pow(l,2) - Math.Pow(x1 - x2,2));
+
+                    brezenhem(bmp, x1, x2, y1, y2);
+                pictureBox1.Image = bmp;
+                await Task.Delay(1);
+            }
+        }
+
+        public void brezenhem(Bitmap bitmap, int x1, int x2, int y1, int y2)
+        {
+            int Px = Math.Abs(x2 - x1);
+            int Py = Math.Abs(y2 - y1);
+            int signX = x1 < x2 ? 1 : -1;
+            int signY = y1 < y2 ? 1 : -1;
+            int E = Px - Py;
+            bitmap.SetPixel(x1, y1, Color.Black);
+            bitmap.SetPixel(x2, y2, Color.Black);
+            while (x1 != x2 || y1 != y2)
+            {
+                bitmap.SetPixel(x1, y1, Color.Black);
+                int E2 = E * 2;
+                if (E2 > -Py)
+                {
+                    E -= Py;
+                    x1 += signX;
+                }
+                if (E2 < Px)
+                {
+                    E += Px;
+                    y1 += signY;
+                }
+            }
+        }
+
+    }
+}
